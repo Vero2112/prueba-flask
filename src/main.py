38377a8b0369df60d4221a8a3d1c7bd9826a7020
Vero2112 <verorodriguezruiz21@gmail.com>
2022-06-23 17:54:18 +0000

    prueba

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Task
 #from models import Person
 
 app = Flask(__name__)
@@ -20,26 +20,110 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+# todos = [
+#     { "text": "My first task", "done": False }
+# ]
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+# generate_sitemap con todos los endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+    # return "Hola!"
+
+
+@app.route('/task', methods=['GET'])
+def get_task():
+    # json_text = jsonify(todos) learnpack devuelve asi las task, no mapea
+    # no puedo retornarla directamente, necesito serializarla, hay que transoformala en json. necesito llamar a la funcion list 
+    # y se mapea. En vez de utilizar una función flecha equiv función lambda y va a llamar al serializador y me va
+    # a retornar el serializador
+# esta Task es line 23 models?
+    tasks = Task.query.all()
+    all_tasks = list(map(lambda task: task.serialize(), tasks))
+    # print (tasks)
+    # print("hola")
+    # print(all_tasks)
+    return jsonify(all_tasks)
+    # return json_text
+
+@app.route('/task', methods=['POST'])
+def create_task():
+    # request_body = request.data que significa?
+# We already used request.json for that, since we know that the request will be in format application/json. If that is not known, you may want to use request.get_json(force=True) to ignore the content type and treat it like json.
+    body = request.get_json()
+    print(body)
+# body es un diccionario equivalente a un objeto en JS, text es la key
+    task = Task(text=body["text"], done= False)
+# Una vez instanciada la clase, agrego objeto a mi base de datos
+    db.session.add(task)
+# confirmamos añadir tarea a mi base de datos, pero no la puedo retornar directamente, para eso necesito serializar
+    db.session.commit()
+# coge un objeto, tipo task y me lo transforma en un diccionario
+# models line 33-37, pq no le ponemos el parametro self?
+    return jsonify(task.serialize())
+
+@app.route('/task/<int:task_id>', methods=['PUT'])
+def refresh_task(task_id):
+    task = Task.query.get(task_id)
+    # print("hola")
+    # print(task)
+    # print(body)
+    if task is None:
+        raise APIException("Tarea no encontrada", 404)
+    body = request.get_json()
+    if not ("done" in body):
+        raise APIException("Tarea no encontrada", 404)
+    task.done = body["done"]
+    db.session.commit()
+    return jsonify(task.serialize())
+
+@app.route('/task/<int:task_id>', methods=['GET'])
+def get_refresh_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        raise APIException("Tarea no encontrada", 404)
+    return jsonify(task.serialize())
+
+@app.route('/task/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if task is None:
+        raise APIException("Tarea no encontrada", 404)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify(task.serialize())
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    # response_body = {"msg": "Hello, this is your GET/user response"}
+    # return jsonify(response_body), 200
+    users = User.query.all()
+    all_users = list(map(lambda user: user.serialize(), users))
+# todos = db.session.query(Todo)
+# list_todo = []
+# for todo in todos:
+# list_todo.append(todo.serialize())
+# return list_todo
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(all_users), 200
 
-    return jsonify(response_body), 200
+@app.route('/user', methods=['POST'])
+def create_user():
+   
+    body = request.get_json()
+    print(body)
+    user = User(email=body["email"], is_active=True, password="******")
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.serialize())
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
+
--- src/models.py
+++ src/models.py
@@ -8,12 +8,34 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
+# transforma la clase en un string, similar a template string de JS
     def __repr__(self):
         return '<User %r>' % self.username
 
+# mapeo, transforma el objeto en un diccionario donde tenga key.id con su id, ya no devuelve el objeto (la instancia de una clase) sino que devuelve un diccionario
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(120), unique=True, nullable=False)
+    done = db.Column(db.Boolean(), unique=False, nullable=False)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # user = db.relationship(User)
+
+# transforma la clase en un string, similar a template string de JS
+    def __repr__(self):
+        return '<Task %r>' % self.text
+
+# mapeo, transforma el objeto en un diccionario donde tenga key.id con su id, ya no devuelve el objeto (la instancia de una clase) sino que devuelve un diccionario
+    def serialize(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "done": self.done,
             # do not serialize the password, its a security breach
         }
